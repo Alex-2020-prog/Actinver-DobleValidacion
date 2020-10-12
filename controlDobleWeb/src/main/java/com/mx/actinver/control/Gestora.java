7ec3e2a5e4f862383bb10ca967d7964e8d06818d
Alex-2020-prog <aoezco@gmail.com>
2020-10-12 13:42:10 -0500

    Combios en flujo y pantallas

--- controlDobleWeb/src/main/java/com/mx/actinver/control/Gestora.java
+++ controlDobleWeb/src/main/java/com/mx/actinver/control/Gestora.java
@@ -3,6 +3,7 @@ package com.mx.actinver.control;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
 import com.mx.actinver.model.Contacto;
@@ -13,11 +14,13 @@ import lombok.Data;
 
 @Data
 @ManagedBean(name = "dtGestora")
+@ApplicationScoped
 public class Gestora {
 	private Fondo fondo = new Fondo();
 	private List<Contacto> contactos = null;
 	private Contacto contacto = new Contacto();
 	private GestoraService service = new GestoraService();
+	private String imgStep="/controlDobleWeb/resources/images/altaFondo/AA-ContratosMaker.JPG";
 
 	@PostConstruct
 	private void ini() {
@@ -28,4 +31,7 @@ public class Gestora {
 		return service.tiposContacto();
 	}
 
+	public void guardar() {
+		this.imgStep = "/controlDobleWeb/resources/images/altaFondo/AB-ContratosChk.JPG";
+	}
 }
